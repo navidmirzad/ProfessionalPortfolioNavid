d35778ca286aa8c755f4e1af2c7212a1aa899cca
navidmirzad <navi0221@stud.kea.dk>
2023-08-29 17:52:03 +0200

    first commit

---
+++ src/main/java/com/example/professionalportfolionavid/ProfessionalPortfolioNavidApplication.java
@@ -0,0 +1,13 @@
+package com.example.professionalportfolionavid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessionalPortfolioNavidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfessionalPortfolioNavidApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/professionalportfolionavid/ProfessionalPortfolioNavidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.professionalportfolionavid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessionalPortfolioNavidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
